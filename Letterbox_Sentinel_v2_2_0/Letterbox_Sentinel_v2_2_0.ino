35bf057fc02cf4baf54c39bf4c6378f19e5be025
Darksplat <darksplat@gmail.com>
2026-07-27 09:23:54 +1000

    Add compile-ready Letterbox Sentinel v2.2.0 sketch

---
+++ Letterbox_Sentinel_v2_2_0/Letterbox_Sentinel_v2_2_0.ino
@@ -0,0 +1,14 @@
+/*
+  Letterbox Sentinel v2.2.0
+
+  This sketch is split into sequential include files in this folder so the
+  complete public source can be maintained without exposing private network
+  credentials. Arduino compiles the included source as one translation unit.
+
+  Before compiling, edit the USER SETTINGS at the top of part01.inc.
+*/
+
+#include "part01.inc"
+#include "part02.inc"
+#include "part03.inc"
+#include "part04.inc"
